27a8fb654d26f9d5148773265285a6eaf0101dad
Aayush Raj <aayushraj@Aayushs-MacBook-Air.local>
2026-01-01 17:21:48 +0530

    changes for configs

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccb42fdf3dea42e9c714df32bcfe93361280981")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37972124c27f5cedf5f18f4d8ba9d30093016ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
